f60439f762e0ce4b86c5b0da0b749fdce88ceaf2
Mohamed Moustafa <m.moustafa@mail.com>
2026-03-06 11:45:58 -0800

    checkpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,8 +13,6 @@ function App() {
 
   return (
     <div className="flex flex-col h-screen bg-zinc-900 text-zinc-100">
-      <header className="border-b border-zinc-700 px-4 py-3">
-      </header>
       <RagStatusBar status={ragStatus} />
       <div className="flex flex-1 min-h-0">
         <ChatWindow
